a09ca784adaee00603fc086d824f21d7c2e4a6f0
Grigoriy Demidovich <grig.demidovich@gmail.com>
2020-10-25 12:39:01 +0200

    wip

--- js/script1.js
+++ js/script1.js
@@ -2,8 +2,19 @@ let balance = 10000
 const payment = 2000
 console.log(`Сумма заказа ${payment} кредитов. Проверяем кол-во доступных средств на счету:`)
 if (balance >= payment) {
-    balance = balance - payment;
-    console.log(`Всё хорошо! Снимаем деньги, спасибо за покупку!`);
-    console.log(`На счету осталось ${balance} кредитов`);
+	balance = balance - payment
+	console.log(`Всё хорошо! Снимаем деньги, спасибо за покупку!`)
+	console.log(`На счету осталось ${balance} кредитов`)
+} else {
+	console.log('На счету недостаточно средств для проведения операции!')
 }
-// 1-36
+
+const totalExpenses = 20000
+let discount = 0
+if (totalExpenses >= 100 && totalExpenses < 1000) {
+	discount = 2
+	console.log('Бронзовый партнер, скидка 2%')
+} else if (totalExpenses >= 1000 && totalExpenses < 5000) {
+	discount = 5
+	console.log('Серебряный партнер, скидка 5%')
+}
