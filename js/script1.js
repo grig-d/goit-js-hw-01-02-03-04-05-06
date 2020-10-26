53f8e6c7154c15c4ca529c1f30048da1801d5cc8
Grigoriy Demidovich <grig.demidovich@gmail.com>
2020-10-26 02:07:10 +0200

    m1v1 after

--- js/script1.js
+++ js/script1.js
@@ -1,20 +1,32 @@
-let balance = 10000
-const payment = 2000
-console.log(`Сумма заказа ${payment} кредитов. Проверяем кол-во доступных средств на счету:`)
-if (balance >= payment) {
-	balance = balance - payment
-	console.log(`Всё хорошо! Снимаем деньги, спасибо за покупку!`)
-	console.log(`На счету осталось ${balance} кредитов`)
-} else {
-	console.log('На счету недостаточно средств для проведения операции!')
-}
+// Модуль: 1
 
-const totalExpenses = 20000
-let discount = 0
-if (totalExpenses >= 100 && totalExpenses < 1000) {
-	discount = 2
-	console.log('Бронзовый партнер, скидка 2%')
-} else if (totalExpenses >= 1000 && totalExpenses < 5000) {
-	discount = 5
-	console.log('Серебряный партнер, скидка 5%')
-}
+// let balance = 10000
+// const payment = 2000
+// console.log(`Сумма заказа ${payment} кредитов. Проверяем кол-во доступных средств на счету:`)
+// if (balance >= payment) {
+// 	balance = balance - payment
+// 	console.log(`Всё хорошо! Снимаем деньги, спасибо за покупку!`)
+// 	console.log(`На счету осталось ${balance} кредитов`)
+// } else {
+// 	console.log('На счету недостаточно средств для проведения операции!')
+// }
+
+// const totalExpenses = 150;
+// const payment = 500;
+// let discount = 0;
+// if (totalExpenses >= 100 && totalExpenses < 1000) {
+//   discount = 0.02;
+//   console.log('Бронзовый партнер, скидка 2%');
+// } else if (totalExpenses >= 1000 && totalExpenses < 5000) {
+//   discount = 0.05;
+//   console.log('Серебряный партнер, скидка 5%');
+// } else if (totalExpenses >= 5000) {
+//   discount = 0.1;
+//   console.log('Золотой партнер, скидка 10%');
+// } else {
+//   console.log('У вас ещё нет партнерской скидки');
+// }
+// const finalAmount = payment - payment * discount;
+// console.log(`Оформляем заказ на сумму ${finalAmount} со скидкой ${discount * 100}%`);
+
+
