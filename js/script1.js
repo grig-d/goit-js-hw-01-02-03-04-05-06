897f35193e9a2886b166d08e36f3ec7d29ae43ad
Grigoriy Demidovich <grig.demidovich@gmail.com>
2020-10-26 19:39:15 +0200

    wip

--- js/script1.js
+++ js/script1.js
@@ -33,3 +33,36 @@
 // const category = (age >= 18) ? 'adult' : 'child';
 // console.log(category);
 
+const stars = 3;
+if (stars === 1)
+
+
+
+
+
+
+// Задача 1-1
+// const name = '«Генератор защитного поля»'; // Write code on this line
+// let price = 1000; // Write code on this line
+// const firstMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; // Write code on this line
+// price = 2000;
+// const secondMessage = `Выбран ${name}, цена за штуку ${price} кредитов`;
+
+// Задача 1-2
+// const message = invoice > stock ? 'На складе недостаточно товаров!' : 'Заказ оформлен, с вами свяжется менеджер';
+
+// Задача 1-3
+// if (userPassword === null) {
+//   message = CANCELED_BY_USER;
+// } else if (userPassword === ADMIN_PASSWORD) {
+//   message = ACCESS_IS_ALLOWED;
+// } else {
+//   message = ACCESS_DENIED;
+// }
+
+// Задача 1-4
+let totalPrice = orderPieces * pricePerDroid; // Write code on this line
+let balanceCredit = credits - totalPrice; // Write code on this line
+let message;
+if (orderPieces === null) {message = CANCELED_BY_USER;} else if (orderPieces >= 1 && balanceCredit >= 0) {message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceCredit} кредитов`;}
+
