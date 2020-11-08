3b0a9b2556a046746cf0268ebba06b6f9e06150e
Grigoriy Demidovich <grig.demidovich@gmail.com>
2020-11-08 05:51:17 +0200

    module-2-webinar-3

--- js/script1.js
+++ js/script1.js
@@ -63,6 +63,144 @@
 //   }
 // } while (userInput !== 5);
 
+// cycle for of
+// const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
+// const nameToFind = 'Poly';
+// let message = 'is not here';
+// for (const friend of friends) {
+//   if (nameToFind === friend) {
+//     message = 'is here';
+//     break;
+//   }
+// }
+// console.log(`${nameToFind} ${message}`);
+
+// const board = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+// console.log(board[0][1]);
+
+// split & join
+// const a = 'Hell Patrol';
+// const b = a.split(' ');
+// const c = b.join('_')
+// console.log(a, b, c);
+
+// arrays
+// const a = [1, 2, 3];
+// const b = a;
+// const c = [1, 2, 3];
+// console.log(a, b, c, a === b, a === c);
+
+// push & pop
+// const a = [1, 2, 3];
+// a.push(4, 5, 6);
+// a.pop();
+// console.log(a);
+
+// slugify
+// const title = 'Metal Ballads Collection';
+// console.log(title);
+// const normalizedTitle = title.toLowerCase();
+// console.log(normalizedTitle);
+// const splitTitle = normalizedTitle.split(' ');
+// console.log(splitTitle);
+// const slugTitle = splitTitle.join('-');
+// console.log(slugTitle);
+// or
+// const title = 'Metal Ballads Collection';
+// const slug = title.toLowerCase().split(' ').join('-');
+// console.log(slug);
+// or
+// const title = 'Metal Ballads Collection';
+// const words = title.toLowerCase().split(' ');
+// let slug = '';
+// for (const word of words) {
+//     slug += word + '-';
+// }
+// slug = slug.slice(0, (slug.length - 1));
+// console.log(slug);
+// or
+// const title = 'Metal Ballads Collection';
+// const words = title.toLowerCase().split(' ');
+// let slug = '';
+// for (let i = 0; i < words.length; i += 1) {
+//     const isNotLastItem = i < words.length - 1;
+//   slug += isNotLastItem ? words[i] + '-' : words[i];
+// }
+// console.log(slug);
+
+// includes
+// const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
+// const query = 'Poly';
+// message = friends.includes(query) ? 'is here' : 'is absent';
+// console.log(`${query} ${message}`);
+
+// concat
+// const oldHellPatrol = ['Spirit Crusher', 'Death Dealer', 'Storm Bringer'];
+// const addHellPatrol = ['Soul Stealer', 'Hellrider', 'Painkiller'];
+// console.log(oldHellPatrol);
+// console.log(addHellPatrol);
+// const HellPatrol = oldHellPatrol.concat(addHellPatrol);
+// console.log(HellPatrol);
+
+// splice to remove
+// const HellPatrol = [
+//   'Spirit Crusher',
+//   'Death Dealer',
+//   'Storm Bringer',
+//   'Soul Stealer',
+//   'Hellrider',
+//   'Painkiller',
+// ];
+// console.log(HellPatrol);
+// const demonToRemove = 'Hellrider';
+// console.log(demonToRemove);
+// const index = HellPatrol.indexOf(demonToRemove);
+// console.log(index);
+// HellPatrol.splice(index, 1);
+// console.log(HellPatrol);
+
+// splice to add
+// const HellPatrol = [
+//   'Spirit Crusher',
+//   'Death Dealer',
+//   'Storm Bringer',
+//   'Soul Stealer',
+//   'Painkiller',
+// ];
+// console.log(HellPatrol);
+// const demonToAdd = 'Hellrider';
+// const index = 4;
+// HellPatrol.splice(index, 0, demonToAdd);
+// console.log(HellPatrol);
+
+// splice to replace
+// const theHellPatrol = [
+//   'Spirit Crusher',
+//   'Death Dealer',
+//   'Storm Bringer',
+//   'Soul Stealer',
+//   'Painkiller',
+// ];
+// console.log(theHellPatrol);
+// const demonToAdd = 'Hellrider';
+// const demonToReplace = 'Storm Bringer';
+// const index = theHellPatrol.indexOf(demonToReplace);
+// theHellPatrol.splice(index, 1, demonToAdd);
+// console.log(theHellPatrol);
+
+// slice
+// const theHellPatrol = [
+//   'Spirit Crusher',
+//   'Death Dealer',
+//   'Storm Bringer',
+//   'Soul Stealer',
+//   'Painkiller',
+// ];
+// console.log(theHellPatrol.slice(0,3));
 // ------------------------------------------------------------------------------------------------
 
 // Задача 1-1
@@ -135,5 +273,3 @@
 // console.log(message);
 
 // ------------------------------------------------------------------------------------------------
-// const a = b || c ? b + c : b - c;
-
