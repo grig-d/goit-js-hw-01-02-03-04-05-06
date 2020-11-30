632e5cf04fceecc618c4e97f9a202e25e8bd53f5
Grigoriy Demidovich <grig.demidovich@gmail.com>
2020-11-30 03:23:08 +0200

    wip

--- js/main.js
+++ js/main.js
@@ -607,15 +607,37 @@
 // };
 // higherOrderFunction(printMessage);
 
+// // переменной adder присваивается описание функции и затем её можно использовать как функцию
+// let val = 7;
+// function createAdder() {
+//   function addNumbers(a, b) {
+//     let ret = a + b;
+//     return ret;
+//   }
+//   return addNumbers;
+// }
+// let adder = createAdder();
+// // console.log(adder);
+// let sum = adder(val, 8);
+// console.log('example of function returning a function: ', sum);
 
-let val = 7;
-function createAdder() {
-  function addNumbers(a, b) {
-    let ret = a + b;
-    return ret;
-  }
-  return addNumbers;
+
+
+
+
+
+
+
+function createCounter() {
+  let counter = 0;
+  const myFunction = function () {
+    counter = counter + 1;
+    return counter;
+  };
+  return myFunction;
 }
-let adder = createAdder();
-let sum = adder(val, 8);
-console.log('example of function returning a function: ', sum);
+const increment = createCounter();
+const c1 = increment();
+const c2 = increment();
+const c3 = increment();
+console.log('example increment', c1, c2, c3);
