f0bdad804003eeaf343e4b7ce29b57a1adfe20c4
Grigoriy Demidovich <grig.demidovich@gmail.com>
2020-12-07 04:41:30 +0200

    hw-04 done

---
+++ js/hw-05.js
@@ -0,0 +1,16 @@
+// https://homework-js-autocheck.goit.ua/?token=e67afab47edc86c16faa101e36f4bbc165f5f19c4634c61fe67897feba9b9227&block=892345q58
+
+// // Задача 5-1
+ // Write code under this line
+  
+
+//console.log(typeof Account.prototype.getInfo);
+// 'function'
+
+// const mango = new Account( 'Mangozedog', 'mango@dog.woof');
+//console.log(mango.getInfo()); 
+// 'login : Mangozedog, email: mango@dog.woof'
+
+// const poly = new Account( 'Poly', 'poly@mail.com');
+//console.log(poly.getInfo());
+// 'login : Poly, email: poly@mail.com'
