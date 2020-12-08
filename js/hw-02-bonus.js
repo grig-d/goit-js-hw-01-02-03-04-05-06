6bb490a4e949442a0b280cd555483203a5220d03
Grigoriy Demidovich <grig.demidovich@gmail.com>
2020-12-09 01:12:33 +0200

    hw-04 completed

--- js/hw-02-bonus.js
+++ js/hw-02-bonus.js
@@ -6,11 +6,42 @@
 // }
 // console.log(slugify('Как стать JUNIOR разработчиком за ДВЕ НЕДЕЛИ'));
 
-// 16
+// // 16
 // function makeArray(firstArray, secondArray, maxLength) {
 //     // Пиши код ниже этой строки
 // return firstArray.concat(secondArray).slice(0, maxLength);
 //     // Пиши код выше этой строки
 //   }
 // console.log(makeArray(['Манго', 'Поли'], ['Аякс', 'Челси'], 3)); // ['Манго', 'Поли', 'Аякс']
+
+// // 21
+// function findLongestWord(string) {
+//   // Пиши код ниже этой строки
+//   const arr = string.split(' ');
+//   let longestWord = arr[0];
+//   for (let i = 1; i < arr.length; i += 1) {
+//     if (arr[i].length > longestWord.length) {
+//       longestWord = arr[i];
+//     }
+//   }
+//   return longestWord;
+//   // Пиши код выше этой строки
+// }
+// findLongestWord('The quick brown fox  jumped over the lazy dog'); // jumped
+
+// // 23
+// function filterArray(numbers, value) {
+//   // Пиши код ниже этой строки
+//   const filteredArray = [];
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] > value) {
+//       filteredArray.push(numbers[i]);
+//     }
+//     }
+//     return filteredArray;
+//   // Пиши код выше этой строки
+// }
+// filterArray([1, 2, 3, 4, 5], 3); // [4, 5]
+
+
 
