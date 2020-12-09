09126dc06fc46dd65068a734d966618ac37f5531
Grigoriy Demidovich <grig.demidovich@gmail.com>
2020-12-09 17:32:31 +0200

    hw-03-bonus-wip

---
+++ js/hw-03-bonus.js
@@ -0,0 +1,24 @@
+// https://homework-js-autocheck.goit.ua/?token=e67afab47edc86c16faa101e36f4bbc165f5f19c4634c61fe67897feba9b9227&block=vwxyz0169
+
+// // 4
+// const apartment = {
+//     imgUrl: 'https://via.placeholder.com/640x480',
+//     descr: 'Просторная квартира в центре',
+//     rating: 4,
+//     price: 2153,
+//     tags: ['premium', 'promoted', 'top'],
+//     owner: {
+//       name: 'Генри',
+//       phone: '982-126-1588',
+//       email: 'henry.carter@aptmail.com',
+//     },
+//   };
+//   // Пиши код ниже этой строки
+//   const ownerName = apartment.owner.name;
+//   const ownerPhone = apartment.owner.phone;
+//   const ownerEmail = apartment.owner.email;
+//   const numberOfTags = apartment.tags.length;
+//   const firstTag = apartment.tags[0];
+//   const lastTag = apartment.tags[numberOfTags - 1];
+//   // Пиши код выше этой строки
+
