3c9c388cde4a90624ffedf5ef10c14b53e1123ec
Grigoriy Demidovich <grig.demidovich@gmail.com>
2020-12-10 03:37:11 +0200

    wip

--- js/hw-03-bonus.js
+++ js/hw-03-bonus.js
@@ -22,3 +22,19 @@
 //   const lastTag = apartment.tags[numberOfTags - 1];
 //   // Пиши код выше этой строки
 
+// // 10
+// const apartment = {
+//   descr: 'Просторная квартира в центре',
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// // Пиши код ниже этой строки
+// for (const key in apartment) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+// }
+// console.log(keys);
+// console.log(values);
+
