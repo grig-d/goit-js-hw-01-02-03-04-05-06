18d90f9b10a4ad0e9e9d35589da954019d570ef1
Grigoriy Demidovich <grig.demidovich@gmail.com>
2020-12-12 03:12:14 +0200

    wip

--- js/hw-03-bonus.js
+++ js/hw-03-bonus.js
@@ -118,3 +118,14 @@
 // console.log(calculateTotalPrice('Бластер')); //0
 // console.log(calculateTotalPrice('Захват')); //10800
 
+// // деструктуризация
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Пиши код ниже этой строки
+// const { yesterday, today, tomorrow } = highTemperatures;
+// // Пиши код выше этой строки
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+// console.log(meanTemperature); //29
