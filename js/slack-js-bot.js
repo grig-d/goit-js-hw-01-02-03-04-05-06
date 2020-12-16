a8e9c176ac0b1fe598322481fc3e0124a7831f32
Grigoriy Demidovich <grig.demidovich@gmail.com>
2020-12-17 00:56:17 +0200

    wip

---
+++ js/slack-js-bot.js
@@ -0,0 +1,11 @@
+/*
+https://www.npmjs.com/package/webpack-serve
+Если не работает dev - server, предпримите следующие шаги:
+Delete package-lock.json file.
+ Change following in package.json.
+"webpack": "^4.32.2",
+"webpack-cli": "^3.3.0",
+"webpack-dev-server": "^3.11.0"
+Run npm install
+*/
+
