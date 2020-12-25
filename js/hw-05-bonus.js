7638860d75302638ba4eac317d1d679f0646808e
Grigoriy Demidovich <grig.demidovich@gmail.com>
2020-12-25 18:53:43 +0200

    wip 5-bonus

--- js/hw-05-bonus.js
+++ js/hw-05-bonus.js
@@ -144,28 +144,29 @@
 //   }
 // }
 
-// Задача 12
-function Storage(items) {
-  this.items = items;
-}
+// // Задача 12
+// class Storage {
+//   #items;
+//   constructor(items) {
+//     this.#items = items;
+//   }
+//   getItems() {
+//     return this.#items;
+//   }
+//   addItem(newItem) {
+//     this.#items.push(newItem);
+//   }
+//   removeItem(item) {
+//     const itemIndex = this.#items.indexOf(item);
+//     this.#items.splice(itemIndex, 1);
+//   }
+// }
+// // Пиши код выше этой строки
+// const storage = new Storage(['Нанитоиды', 'Пролонгер', 'Антигравитатор']);
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
+// storage.addItem('Дроид');
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
+// storage.removeItem('Пролонгер');
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
 
-Storage.prototype.getItems = function () {
-  return this.items;
-};
-
-Storage.prototype.addItem = function (newItem) {
-  this.items.push(newItem);
-};
-
-Storage.prototype.removeItem = function (item) {
-  const itemIndex = this.items.indexOf(item);
-  this.items.splice(itemIndex, 1);
-};
-
-// Пиши код выше этой строки
-const storage = new Storage(["Нанитоиды", "Пролонгер", "Антигравитатор"]);
-console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
-storage.addItem("Дроид");
-console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
-storage.removeItem("Пролонгер");
-console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
+// Задача 13
