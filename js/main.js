82ad5f033c5802bbe7cdbc634542a4162410ce1e
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-01-01 05:21:33 +0200

    wip module 6

--- js/main.js
+++ js/main.js
@@ -1366,3 +1366,38 @@
 // const greaterThenTwoR = numbersR.filter(num => num > 2);
 // console.log(greaterThenTwoR);
 
+// // map
+// const players = [
+//   { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//   { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//   { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//   { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//   { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+// ];
+// // получаем (новый) массив всех имен
+// const playersNames = players.map(function (player) {
+//   return player.name;
+// });
+// console.log(playersNames);
+// // увеличиваем points каждому игроку и получаем (новый) массив
+// const updatedPoints = players.map(function (player) {
+//   return {
+//     ...player,
+//     points: player.points + player.points * 0.1,
+//   };
+// });
+// console.log(updatedPoints);
+// // увеличиваем кол-во часов игрока по id
+// const playerIdToUpdate = 'player-3';
+// const updatedPlayers = players.map(function (player) {
+//   if (player.id === playerIdToUpdate) {
+//     return {
+//       ...player,
+//       timePlayed: player.timePlayed + 50,
+//     };
+//   }
+//   return player;
+// });
+// console.log(updatedPlayers);
+
+// 26-00
