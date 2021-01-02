9b2ff8b3946fb853b1e2c7aa88f586c87adc64c9
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-01-02 19:36:31 +0200

    wip 6 module

--- js/main.js
+++ js/main.js
@@ -1400,4 +1400,72 @@
 // });
 // console.log(updatedPlayers);
 
-// 26-00
+// // filter
+// const numbers = [5, 10, 15, 20, 25];
+// const filteredNumbers = numbers.filter(number => {
+//     return number > 15;
+// });
+// console.log(filteredNumbers);
+
+// // filter
+// const players = [
+//   { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//   { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//   { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//   { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//   { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+// ];
+// const onlinePlayers = players.filter(player => {
+//   return player.online;
+// });
+// console.log(onlinePlayers);
+// const offlinePlayers = players.filter(player => {
+//   return !player.online;
+// });
+// console.log(offlinePlayers);
+// const hardcorePlayers = players.filter(player => {
+//   return player.timePlayed > 250;
+// });
+// console.log(hardcorePlayers);
+
+// // find
+// const numbers = [5, 10, 15, 20, 25];
+// const number = numbers.find(n => {
+//   return n === 15;
+// });
+// console.log(number);
+
+// // find
+// const numbers = [5, 10, 15, 20, 25];
+// const number = numbers.find(n => {
+//   return n > 15;
+// });
+// console.log(number);
+
+// // find
+// // найти игрока по id
+// const players = [
+//   { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//   { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//   { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//   { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//   { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+// ];
+// const playerToFind = 'player-3';
+// const player = players.find((player) => { return player.id === playerToFind; });
+// console.log(player);
+
+// // every
+// const players = [
+//   { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//   { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//   { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//   { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//   { id: 'player-5', name: 'Chelsy', timePlayed: 280, points: 48, online: true },
+// ];
+// const isAllPlayersOnline = players.every(player => { return player.online; });
+// console.log(isAllPlayersOnline);
+// const averagedInPlayTime = players.every(player => { return player.timePlayed > 100; });
+// console.log(averagedInPlayTime);
+
+56-00
